6eec4e3298e1a5000bab5ec3d35795fca6ff26d3
Matias Valle <matias.valle@mi.unc.edu.ar>
2022-10-12 00:07:58 -0300

    Se agrego la verificacion de datos con la base de datos(Pony)

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import email
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import jwt
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
+
+
+#Se importa la base de datos
+from models import db
+
  
 # TODO: put some of this in .env file
 SECRET_KEY = "my_secret_key"
@@ -12,7 +18,8 @@ FRONTEND_URL = "http://localhost:3001"
  
 app = FastAPI()
  
- 
+
+# Usuario generico de prueba
 dummy_user = {
     "email": "famaf01",
     "password": "nuevofamaf"
@@ -39,15 +46,32 @@ class LoginItem(BaseModel):
  
 @app.get("/")
 def read_root():
-    return {"Bienvenido de nuevo"}
+    return {"Bienvenido de nuevo!!!"}
  
  
 @app.post("/login")
 async def login_user(login_item: LoginItem):
     data = jsonable_encoder(login_item)
-    if dummy_user['email'] == data['email'] and dummy_user['password'] == data['password']:
-        encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-        return {'token': encoded_jwt}
-    else:
-        return {'error': 'Login failed'}
+    with db_session:
+        if User.exist(email == data['email']):
+            currentUser = User.select(lambda u: u.email==data['email'])
+            if currentUser.password == data['password']:
+                encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+                return {'token': encoded_jwt}
+            else:
+                return  {'error': 'Password incorrecta'}
+        elif not(User.exist(email == data['email'])):
+            return {'error': 'No existe el usuario'}
+        else:
+            return {'error': 'Login failed'}
 
+
+
+# @app.post("/login")
+# async def login_user(login_item: LoginItem):
+#     data = jsonable_encoder(login_item)
+#     if dummy_user['email'] == data['email'] and dummy_user['password'] == data['password']:
+#         encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+#         return {'token': encoded_jwt}
+#     else:
+#         return {'error': 'Login failed'}
