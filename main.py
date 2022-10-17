56a65b5969a6d8e1580471244867fa12ead160dd
GonzaloGarciaZurlo <gonzalogarciazurlo@gmail.com>
2022-10-17 15:48:37 -0300

    ticket terminado con sus test

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import jwt
+from fastapi import Request
+from models import *
+
+# TODO: put some of this in .env file
+SECRET_KEY = "my_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 800
+FRONTEND_URL = "http://localhost:3001"
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_current_user(data):
+    current_user_info = jwt.decode(data, SECRET_KEY, algorithm=ALGORITHM)
+    current_user = User.get(email=current_user_info["email"])
+    return current_user
+
+
+def get_user_robots(User):
+    list_of_robots = []
+    robots = Robot.select(lambda r: r.user == User).   order_by(
+        desc(Robot.name), Robot.id)[:]
+    for i in robots:
+        list_of_robots.append({"id": i.id, "name": i.name})
+
+    return list_of_robots
+
+origins = {
+    "http://localhost",
+    FRONTEND_URL,
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/robots")
+async def list_robots(request: Request):
+    with db_session:
+        token = request.headers.get("Authotization")
+        if token[0:7] != "Bearer ":
+            return {'error': 'Invalid header'}
+        else:
+            token = token[7:]
+        curent_user = get_current_user(token)
+        if curent_user == None:     # no existe el usuario en la bd
+            return {'error': 'Invalid X-Token header'}
+        else:
+            user_robots = get_user_robots(curent_user)
+            return {'robots':user_robots}
+with db_session:
+   User(username = "pedro", email = "famaf01@gmail.com", password = "nuevofamaf", is_validated = True)
---
+++ test_main.py
@@ -0,0 +1,49 @@
+from .main import *
+from fastapi.testclient import TestClient
+import jwt
+
+client = TestClient(app)
+
+SECRET_KEY = "my_secret_key"
+ALGORITHM = "HS256"
+dummy_user = {
+    "email": "famaf01@gmail.com",
+    "password": "nuevofamaf"
+}
+dummy_user2 = {
+    "email": "famaf02@gmail.com",
+    "password": "nuevofamaf"
+}
+encoded_jwt = jwt.encode(dummy_user, SECRET_KEY, algorithm=ALGORITHM)
+encoded = encoded_jwt.decode("utf-8")
+
+encoded_jwt2 = jwt.encode(dummy_user2, SECRET_KEY, algorithm=ALGORITHM)
+encoded2 = encoded_jwt2.decode("utf-8")
+
+
+
+#test
+def test_read_main():
+    response = client.get(
+        "/robots",
+        headers={"Authotization": "Bearer " + encoded}
+    )
+    assert response.status_code == 200
+    assert response.json() == {"robots":[{'id': 3, 'name': 'example3'}, {'id': 6, 'name': 'example3'}, {'id': 2, 'name': 'example2'},
+     {'id': 5, 'name': 'example2'}, {'id': 1, 'name': 'example1'}, {'id': 4, 'name': 'example1'}]}
+
+def test_bad_token_read_main():
+    response = client.get(
+        "/robots",
+        headers={"Authotization": "Bearer " + encoded2}
+    )
+    assert response.status_code == 200
+    assert response.json() == {'error': 'Invalid X-Token header'}
+
+def test_bad_header_read_main():
+    response = client.get(
+        "/robots",
+        headers={"Authotization": encoded}
+    )
+    assert response.status_code == 200
+    assert response.json() == {'error': 'Invalid header'}
