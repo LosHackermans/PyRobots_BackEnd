ba443629672d44b1a3b5065bd4b89092c511cbe5
santimc <santimonserr@hotmail.com>
2022-10-18 08:29:01 -0300

    Revert "Feature pyr 54 end point listar robots"

--- test_main.py
+++ test_main.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-from .main import *
-from fastapi.testclient import TestClient
-import jwt
-
-=======
 from fastapi.testclient import TestClient
 from .main import *
 import jwt
 
 
->>>>>>> develop
 client = TestClient(app)
 
 SECRET_KEY = "my_secret_key"
@@ -28,35 +21,6 @@ encoded = encoded_jwt.decode("utf-8")
 encoded_jwt2 = jwt.encode(dummy_user2, SECRET_KEY, algorithm=ALGORITHM)
 encoded2 = encoded_jwt2.decode("utf-8")
 
-<<<<<<< HEAD
-
-
-#test
-def test_read_main():
-    response = client.get(
-        "/robots",
-        headers={"authorization": "Bearer " + encoded}
-    )
-    assert response.status_code == 200
-    assert response.json() == {"robots":[{'id': 3, 'name': 'example3'}, {'id': 6, 'name': 'example3'}, {'id': 2, 'name': 'example2'},
-     {'id': 5, 'name': 'example2'}, {'id': 1, 'name': 'example1'}, {'id': 4, 'name': 'example1'}]}
-
-def test_bad_token_read_main():
-    response = client.get(
-        "/robots",
-        headers={"authorization": "Bearer " + encoded2}
-    )
-    assert response.status_code == 200
-    assert response.json() == {'error': 'Invalid X-Token header'}
-
-def test_bad_header_read_main():
-    response = client.get(
-        "/robots",
-        headers={"authorization": encoded}
-    )
-    assert response.status_code == 200
-    assert response.json() == {'error': 'Invalid header'}
-=======
 # test create user
 def test_create_user():
     response = client.post(
@@ -212,4 +176,3 @@ def test_bad_read_main2():
     assert response.status_code == 200
     assert response.json() == {'error': ' incorrect Password'}
 
->>>>>>> develop
