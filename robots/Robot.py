532cec76d002cf9677bd747d883f108e4f21a3b5
Francisco A <fayrolo@mi.unc.edu.ar>
2022-10-26 19:55:06 -0300

    Hechas las funcionalidades más básicas

--- robots/Robot.py
+++ robots/Robot.py
@@ -1,12 +1,42 @@
-    
+from Map import Map
 
 class Robot():
+#Funciones auxiliares, el usuario no las conoce    
+    def __init__(self, pos_x: int, pos_y: int, direction)
+        self.data = {}
+        self.data["pos_x"] = pos_x
+        self.data["pos_y"] = pos_y
+        self.data["health"] = 100
+        self.data["direction"] = 0
+        self.data["velocity"] = 0
+        self.data["cannon"] = True
+        self.data["cannon_degree"] = 0
+        self.data["cannon_distance"] = 0
+        self.data[""] = 
+        
+        
+    def __get_data(self):
+        return self.data
+    
+    def __ready_cannon(self):
+        if not self.data["cannon"]:
+            self.data["cannon"] = True
+            
+    def __spend_cannon(self):
+        self.data["cannon"] = False
 
+    def __es_numero(a):
+        return type(a) == int or type(a) == float
+
+#funciones publicas
     def is_cannon_ready(self):
-        print("llamada a is_cannon_ready")
+        return cannon_ready
     
     def cannon(self, degree, distance):
         print("llamada a cannon(%s, %s)" % (degree, distance))
+        if __es_numero(degree) and __es_numero(distance):
+            self.data["cannon_degree"] = degree
+            self.data["cannon_distance"] = distance
         
     def point_scanner(self, direction, resolution_in_degrees):
         print("llamada a point_scanner(%s, %s)" % (direction, resolution_in_degrees))
@@ -16,16 +46,18 @@ class Robot():
         
     def drive(self, direction, velocity):
         print("llamada a drive(%s, %s)" % (direction, velocity))
+        if __es_numero(direction) and __es_numero(velocity):
+            self.data["direction"] = direction
+            self.data["velocity"] = velocity
         
     def get_direction(self):
-        print("llamada a get_direction")
+        return self.direction
         
     def get_velocity(self):
-        print("llamada a get_velocity")
+        return self.engine_velocity
         
     def get_position(self):
-        print("llamada a get_position")
+        return self.position
         
     def get_damage(self):
-        print("llamada a get_damage")
-    
+        return 100 - self.data["health"]
