ee7f199099b0f9ebe6e961e3e97325849d0b9e88
GonzaloGarciaZurlo <gonzalogarciazurlo@gmail.com>
2022-10-30 15:48:47 -0300

    partidas ya unidas agregadas

--- app/api/list_matches.py
+++ app/api/list_matches.py
@@ -20,18 +20,29 @@ def get_current_user(data):
 def get_matchs(user):
     PartidasDeUsuario = []
     PartidasParaUnirse = []
+    PartidasUnidas = []
     matchs = Match.select() .   order_by(
         desc(Match.name), Match.id)[:]
+    
     for i in matchs:
-        if i.user == user:
-            PartidasDeUsuario.append({"id": i.id, "name": i.name})
-        else:
-            PartidasParaUnirse.append({"id": i.id, "name": i.name})
-    return (PartidasDeUsuario, PartidasParaUnirse)
+        if i.is_joinable:   # si la partida esta llena, o ya comenzo esta variable es false
+        
+            if i.user == user:
+                PartidasDeUsuario.append({"id": i.id, "name": i.name})
+            else:
+                esta_unido = False
+                for j in i.robot_in_matches:
+                    for h in user.robots:
+                        if j.robot == h and i.user != user:
+                            PartidasUnidas.append({"id": i.id, "name": i.name})
+                            esta_unido = True
+                if not esta_unido:       
+                    PartidasParaUnirse.append({"id": i.id, "name": i.name})
+    return (PartidasDeUsuario, PartidasParaUnirse, PartidasUnidas)
 
 
 @router.get("/matches")
-async def list_matchs(request: Request):
+async def list_matches(request: Request):
     with db_session:
         token = request.headers.get("authorization")
         if token[0:7] != "Bearer ":
@@ -42,6 +53,7 @@ async def list_matchs(request: Request):
         if curent_user == None:     # no existe el usuario en la bd
             return {'error': 'Invalid X-Token header'}
         else:
-            PartidasDeUsuario,PartidasParaUnirse = get_matchs(curent_user)
+            PartidasDeUsuario,PartidasParaUnirse,PartidasUnidas = get_matchs(curent_user)
             return {'PartidasDeUsuario': PartidasDeUsuario,
-             "PartidasParaUnirse": PartidasParaUnirse}
+             "PartidasParaUnirse": PartidasParaUnirse,
+             "PartidasUnidas":PartidasUnidas}
--- test/test_list_matches.py
+++ test/test_list_matches.py
@@ -42,12 +42,46 @@ def test_valid_example():
     )
     assert response.status_code == 200
     assert response.json() == {"PartidasDeUsuario": [{'id': id1, 'name': 'match1'}],
-                               "PartidasParaUnirse": [{'id': id3, 'name': 'match3'}, 
-                               {'id': id2, 'name': 'match2'}]}
+                               "PartidasParaUnirse": [{'id': id3, 'name': 'match3'},
+                                                      {'id': id2, 'name': 'match2'}],
+                               "PartidasUnidas": []}
     with db_session:
         delete(m for m in Match if m.name == "match1")
         delete(m for m in Match if m.name == "match2")
         delete(m for m in Match if m.name == "match3")
+        delete(u for u in User if u.username == "ej")
+
+
+def test_full_valid_example():
+    with db_session:
+        User(username="ej", email="pepito@gmail.com",
+             password="abc", is_validated=True)
+        Robot(name="robot", script="abc", user=User[1])
+        Match(name="match1", min_players=2, max_players=4,
+              number_rounds=200, number_games=200, is_joinable=True, user=User[1])
+        Match(name="match2", min_players=2, max_players=4,
+              number_rounds=200, number_games=200, is_joinable=True, user=User.get(username="ej"))
+        Match(name="match3", min_players=2, max_players=4,
+              number_rounds=200, number_games=200, is_joinable=True, user=User.get(username="ej"))
+        Robot_in_match(robot=Robot.get(name="robot"),
+                       games_won=2, games_draw=2, match= Match.get(name="match2"))
+        id1 = (Match.get(name="match1")).id
+        id2 = (Match.get(name="match2")).id
+        id3 = (Match.get(name="match3")).id
+    response = client.get(
+        "/matches",
+        headers={"authorization": "Bearer " + encoded}
+    )
+    assert response.status_code == 200
+    assert response.json() == {"PartidasDeUsuario": [{'id': id1, 'name': 'match1'}],
+                               "PartidasParaUnirse": [{'id': id3, 'name': 'match3'}],
+                               "PartidasUnidas": [{'id': id2, 'name': 'match2'}]}
+    with db_session:
+        delete(m for m in Match if m.name == "match1")
+        delete(m for m in Match if m.name == "match2")
+        delete(m for m in Match if m.name == "match3")
+        delete(r for r in Robot if r.name == "robot")
+        delete(r for r in Robot_in_match if r.games_won == 2)
         delete(u for u in User if u.username == "ej")
 
 
