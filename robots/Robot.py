1fc89cc05b6ba60bba6cec398cd268909568eb53
Francisco A <fayrolo@mi.unc.edu.ar>
2022-10-31 19:42:07 -0300

    Agregados tests, corregidas definiciones de clases

--- robots/Robot.py
+++ robots/Robot.py
@@ -1,15 +1,19 @@
 from Map import Map
 
-class Robot():
+class Robot:
 #Funciones auxiliares, el usuario no las conoce    
-    def __init__(self, pos_x: int, pos_y: int, direction)
+    def __init__(self, pos_x: int, pos_y: int):
         self.data = {}
-        self.data["pos_x"] = pos_x
-        self.data["pos_y"] = pos_y
+        if self.__es_numero(pos_x) and self.__es_numero(pos_y):
+            self.data["pos_x"] = pos_x
+            self.data["pos_y"] = pos_y
+        else:
+            self.data["pos_x"] = 0
+            self.data["pos_y"] = 0
         self.data["health"] = 100
         self.data["direction"] = 0
         self.data["velocity"] = 0
-        self.data["cannon"] = True
+        self.data["cannon_ready"] = True
         self.data["cannon_degree"] = 0
         self.data["cannon_distance"] = 0
         self.data["scanner_direction"] = 0
@@ -17,32 +21,34 @@ class Robot():
         self.data["scanned"] = 0
         
         
-    def __get_data(self):
+    def get_data(self):
         return self.data
     
-    def __ready_cannon(self):
-        if not self.data["cannon"]:
-            self.data["cannon"] = True
+    def ready_cannon(self):
+        if not self.data["cannon_ready"]:
+            self.data["cannon_ready"] = True
             
-    def __spend_cannon(self):
-        self.data["cannon"] = False
+    def spend_cannon(self):
+        was_ready = self.data["cannon_ready"]
+        if was_ready:
+            self.data["cannon_ready"] = False
+        return was_ready
+        
 
-    def __es_numero(a):
+    def __es_numero(self, a):
         return (type(a) == int or type(a) == float) and a >= 0
 
 #funciones publicas
-    def is_cannon_ready(self):
-        return cannon_ready
-    
     def cannon(self, degree, distance):
-        #print("llamada a cannon(%s, %s)" % (degree, distance))
-        if __es_numero(degree) and __es_numero(distance) and degree < 360 and distance < 700:
+        if self.__es_numero(degree) and self.__es_numero(distance) and degree < 360 and distance < 700:
             self.data["cannon_degree"] = degree
             self.data["cannon_distance"] = distance
-        
+            
+    def is_cannon_ready(self):
+        return self.data["cannon_ready"]
+    
     def point_scanner(self, direction, resolution):
-        #print("llamada a point_scanner(%s, %s)" % (direction, resolution_in_degrees))
-        if __es_numero(direction) and __es_numero(resolution) and direction < 360 and resolution <= 10:
+        if self.__es_numero(direction) and self.__es_numero(resolution) and direction < 360 and resolution <= 10:
             self.data["scanner_direction"] = direction
             self.data["scanner_resolution"] = resolution
         
@@ -50,19 +56,18 @@ class Robot():
         return self.data["scanned"]
         
     def drive(self, direction, velocity):
-        #print("llamada a drive(%s, %s)" % (direction, velocity))
-        if __es_numero(direction) and __es_numero(velocity) and direction < 360 and velocity <= 100:
+        if self.__es_numero(direction) and self.__es_numero(velocity) and direction < 360 and velocity <= 100:
             self.data["direction"] = direction
             self.data["velocity"] = velocity
         
     def get_direction(self):
-        return self.direction
+        return self.data["direction"]
         
     def get_velocity(self):
-        return self.engine_velocity
+        return self.data["velocity"]
         
     def get_position(self):
-        return self.position
+        return (self.data["pos_x"], self.data["pos_y"])
         
     def get_damage(self):
         return 100 - self.data["health"]
