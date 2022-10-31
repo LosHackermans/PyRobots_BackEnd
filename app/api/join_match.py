61968297ef71dbf43e1c71981d43a3ebca6b4750
aaguussilva <aaguussilva@gmail.com>
2022-10-31 20:22:58 -0300

    Add tests

--- app/api/join_match.py
+++ app/api/join_match.py
@@ -8,38 +8,12 @@ import jwt
 from pydantic import BaseModel
 import base64
 from app.api.models import *
+# from models import *
 from fastapi.encoders import jsonable_encoder
 from typing import Optional
 import json
 from pony.orm import db_session
 from fastapi import APIRouter
-
-
-
-# TODO: 
-
-# - Tarea
-#       - Los datos del front que intesan son solo el name y password de sala, y el los datos del usuario
-#       - El usuario ve las partidas a las que se puede unir
-#       - Verificar si se puede unir a la partida (is_joinable), pero puede ser que no haga falta
-#       - Luego si es joinable, agregar al usuario a la partida
-#       - Luego verificar si la cantidad de jugadores en la partida super la max_players, si pasa cambiar joinable a False
-
-
-
-
-# Preguntas:
-#     - El front envia el name o el id, el front tiene acceso al id, preguntar al que se encarga de la parte de listar partidas, y en el front cuando la seleccionas??
-#     - Preguntar que pasa si dos usuarios al mismo tiempo intentan unirse o si es posible que eso pase
-#          - Por el tema de verificar si es joinable
-
-# id_match: str
-# or
-# name_match=body.name, 
-
-# password=body.password,
-
-# user=curent_user (user id o username)
 
 router = APIRouter()
 
@@ -48,14 +22,14 @@ router = APIRouter()
 class JoinMatchModel(BaseModel): 
     id_match: int # El front puede enviar id? El nombre y demas puede repetirse
     password_match: str
-    username: str
+    id_robot: str
 
     class Config:
         schema_extra = {
             'example': {
                 "id_match": 1,
                 "password_match": "testPassword",
-                "username": "testUser"
+                "id_robot": 1
             }
         }
 
@@ -64,18 +38,29 @@ class JoinMatchModel(BaseModel):
 async def join_match(match: JoinMatchModel):
  
     with db_session:
-        # Primero busco el match en la bd para extraer los datos, sobre todo si es joinable
-        current_match = Match.get(id=match.id_match)
-        current_user = User.get(username=match.username) # Por ahora dejarlo asi, ver como se recibe al usuario
-        # si se pudo unir es porq el match existe
+        # NOTE: For now the password is not required
+
+        current_match = Match.get(id = match.id_match)
         if current_match == None: # Ver si esto puede pasar
-            pass # El match no exist ?
-        if (current_user == None):
-            pass # No existe Usuario? 
-        if (current_match.password != match.password_match):
-            pass # Password incorrecta
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Match does not exist")
 
-        # TODO: Agregar a Match el usuario
+        if current_match.is_joinable == False:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Unable to join match")
+
+        current_robot = Robot.get(id = match.id_robot)
+        if current_robot == None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Robot does not exist")
+
+        Robot_in_match(robot = current_robot, games_won = 0, games_draw = 0, match = current_match)
+
+        # NOTE: No puedo obtener la cantidad de usuarios para actualizar el campo is_joinable
+        # number_of_robots =  select(r for r in Robot_in_match if r.match == current_match)
+        # if len(number_of_robots) >= current_match.max_players:
+        #     current_match.is_joinable = False
+        
+    return {"message": "User joined successfully"}
 
 
-    return {"message": "User add successfully"}
