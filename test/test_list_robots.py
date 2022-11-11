0262cdfd4c736cc5d4a470ac01242de26403ffcc
aaguussilva <aaguussilva@gmail.com>
2022-11-10 21:01:33 -0300

    Add tests, all passed

--- test/test_list_robots.py
+++ test/test_list_robots.py
@@ -16,11 +16,19 @@ dummy_user2 = {
     "email": "famaf02@gmail.com",
     "password": "nuevofamaf"
 }
+dummy_user3 = {
+    "email": "famaf03@gmail.com",
+    "password": "nuevofamaf"
+}
 encoded_jwt = jwt.encode(dummy_user, SECRET_KEY, algorithm=ALGORITHM)
 encoded = encoded_jwt.decode("utf-8")
 
 encoded_jwt2 = jwt.encode(dummy_user2, SECRET_KEY, algorithm=ALGORITHM)
 encoded2 = encoded_jwt2.decode("utf-8")
+
+# Ver este tema
+encoded_jwt3 = jwt.encode(dummy_user3, SECRET_KEY, algorithm=ALGORITHM)
+encoded3 = encoded_jwt3.decode("utf-8")
 
 
 #test
@@ -76,3 +84,29 @@ def test_invalid_header():
     )
     assert response.status_code == 200
     assert response.json() == {'error': 'Invalid X-Token header'}
+
+
+def test_valid_example_with_statistics():
+    with db_session:
+        user_test = User(username = "test_stats", email = "famaf03@gmail.com",
+             password = "nuevofamaf", is_validated = True)
+        robot_test = Robot(name="robot_1",script="abc",user=User.get(email = "famaf03@gmail.com"))
+        match_test_0 = Match(name="match_0", min_players=2, max_players=4,
+              number_rounds=200, number_games=200, is_joinable=True, user=user_test)
+        match_test_1 = Match(name="match_1", min_players=2, max_players=4,
+              number_rounds=200, number_games=200, is_joinable=True, user=user_test)
+        Robot_in_match(robot = robot_test, games_won = 10, games_draw = 3, match = match_test_0)
+        Robot_in_match(robot = robot_test, games_won = 15, games_draw = 7, match = match_test_1)
+
+    response = client.get(
+        "/robots",
+        headers={"authorization": "Bearer " + encoded3}
+    )
+    assert response.status_code ==  200
+    assert response.json() == {"robots":[{'id': robot_test.id, 'name': robot_test.name, 'avatar': robot_test.avatar, 'games_won': 25, 'games_draw': 10, 'games_played': 2}]}
+    with db_session:
+        delete (u for u in User if u.email == "famaf03@gmail.com")
+        delete(m for m in Match if m.name == "match_0")
+        delete(m for m in Match if m.name == "match_1")
+        delete(r for r in Robot_in_match if r.robot == robot_test)
+        delete(r for r in Robot if r.name == "robot_1")
