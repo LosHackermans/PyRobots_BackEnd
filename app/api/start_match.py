2c282ef22c76816860f6c58f9270b28b8a264116
Usuario Visita <visitante@lab73.famaf.unc.edu.ar>
2022-11-18 14:51:30 -0300

    Listo esqueleto start_match

--- app/api/start_match.py
+++ app/api/start_match.py
@@ -10,14 +10,19 @@ router = APIRouter()
 
 
 @router.post("/start/{match_id}")
-async def start_match():
+async def start_match(match_id):
 
-    match_is_ready_to_start = False
-    if (!match_is_ready_to_start):
-        return {"detail": "The match is not ready yet"}
+    The_Match = Match.get(lambda m: m.id == match_id)
+    match_is_ready_to_start = 1
+    if (match_is_ready_to_start < 2):
+        match_is_ready_to_start = 0
+        for user_in_match in The_Match.robot_in_matches:
+            match_is_ready_to_start += 1
+
+        return {"detail": "The match is not ready to start yet"}
     else:
         #Ejecutar partida, obtener el ganador(ganadores) y devolverlos
-
+        return {"Result": [{"User": "Pepito", "Robot": "Pepitron"}] }
 
 
 
