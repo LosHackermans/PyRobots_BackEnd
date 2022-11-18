ffde36e5c2b6b03d5986ef00930c9f09e4ec2bfb
Usuario Visita <visitante@lab73.famaf.unc.edu.ar>
2022-11-18 14:32:20 -0300

    Agregado esqueleto start_match

--- app/api/start_match.py
+++ app/api/start_match.py
@@ -12,7 +12,11 @@ router = APIRouter()
 @router.post("/start/{match_id}")
 async def start_match():
 
-
+    match_is_ready_to_start = False
+    if (!match_is_ready_to_start):
+        return {"detail": "The match is not ready yet"}
+    else:
+        #Ejecutar partida, obtener el ganador(ganadores) y devolverlos
 
 
 
