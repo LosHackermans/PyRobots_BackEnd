64be0618c3d97ae661afcdc68972d4aff80d2759
Francisco A <fayrolo@mi.unc.edu.ar>
2022-11-18 23:51:35 -0300

    modificada la salida de correr partida para encajar con el front

--- robots/Partida.py
+++ robots/Partida.py
@@ -2,7 +2,6 @@ from robots.Juego import *
 
 class Partida:
     def __init__(self, bot_list, config_partida):
-        print("init de Partida")
         self.bot_list = bot_list
         self.scores, self.players = parse_bot_list(bot_list)        
         self.games = config_partida["games"]
@@ -16,11 +15,15 @@ class Partida:
             self.scores[f"{winner}"] += 1
 
         self.scores["EMPATE"] = 0
+        #Descomentar esto para que haya más de un ganador (o sea, empate de partida)
+        #self.scores["CircleBot"] = 4
+        #self.scores["SquareBot"] = 4
         results = [key for key, value in self.scores.items() if value == max(self.scores.values())]
-        output = []
+        winners_list = []
         for bot in results:
-            output.append((bot, self.players[f"{bot}"]))
-        return output
+            winners_list.append({"user": self.players[f"{bot}"], "robot": bot})
+        
+        return winners_list
 
 
 def parse_bot_list(bot_list):
@@ -37,7 +40,6 @@ def parse_bot_list(bot_list):
 
 
 if __name__=="__main__":
-    print("main de Partida")
     p = Partida(["robots/files/admin/CircleBot.py", "robots/files/admin1/SquareBot.py", "robots/files/admin2/SuperMegaRobot.py"], {"games":10, "rounds":200})
     print(p.run())
     
